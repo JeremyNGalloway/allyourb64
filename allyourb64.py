1a8d924cda8e806cc169c353f50e3e291d996b84
Jeremy Galloway <JeremyNGalloway@gmail.com>
2014-08-29 10:14:05 -0500

    Princification

--- allyourb64.py
+++ allyourb64.py
@@ -1,17 +1,34 @@
-#!/usr/bin/python
-#Jeremy Galloway 2014
-#This script converts a text file *Line By Line* into b64 encoded strings
-#This is often useful for attacking BASIC www auth
-#Simply pass in the source file are the first arguement and redirect STDOUT
+"""
+Reads in a newline-delimited userlist file and passwords file and creates a
+formatted output (user:password) file mapping each user to all passwords,
+base-64-encoded.
+
+"""
 
 import sys
 import base64
 
-import sys
-import base64
+
+def main():
+    """
+    This ingests the input files (a newline-delimited userlist file and a
+    newline-delimited passwords-list file) and creates a formatted output file
+    of matching each user with all passwords.
+
+    The userlist file is assumed to be the first command-line argumnet; the
+    passwords file the second; and the output file the third.
+
+    """
+    with open(sys.argv[1], 'r') as f:
+        userlist = [line.strip() for line in f]
+    with open(sys.argv[2], 'r') as f:
+        pwlist = [line.strip() for line in f]
+    with open(sys.argv[3], 'w') as f:
+        for user in userlist:
+            for pw in pwlist:
+                f.write(base64.b64encode('%s:%s' % (user, pw)) + '\n')
 
 
-for filename in sys.argv[1:]:
-    with open(filename, 'r') as f:
-        for line in f:
-            print base64.b64encode(line.strip())
+# If called on the command-line as a script, launch main function.
+if __name__ == "__main__":
+    main()
